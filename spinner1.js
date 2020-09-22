ee45b47f7b35c3c4d330543671a03a6213e3fc80
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-22 18:38:21 +0000

    Implemented a loading spinner using setTimeOut

---
+++ spinner1.js
@@ -0,0 +1,18 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+let array = ["|","/","-","\\","|","/","-","\\","|"];
+
+let i = 100;
+for(let item of array) {
+  setTimeout(() => {
+    process.stdout.write('\r'+item + "   ");
+  }, i);
+  
+  i += 200;
+}
+
+
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, i);
