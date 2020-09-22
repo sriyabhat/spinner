8550e8983ec246afba6e59025ffd17e99a61ec66
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-22 18:47:12 +0000

    updated spinner.js

--- spinner1.js
+++ spinner1.js
@@ -1,18 +1,43 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
-let array = ["|","/","-","\\","|","/","-","\\","|"];
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
 
-let i = 100;
-for(let item of array) {
-  setTimeout(() => {
-    process.stdout.write('\r'+item + "   ");
-  }, i);
-  
-  i += 200;
-}
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
 
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
 
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
 
 setTimeout(() => {
   process.stdout.write('\n');
-}, i);
+}, 1900);
